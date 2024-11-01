8a4014680247f7142ef03d36ad41cfdf526277cf
Jessie Chang <147798314+jessiechang03@users.noreply.github.com>
2024-11-01 19:55:29 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?PHP
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "flexmatch_database";
+
+    // Create connection
+    $con = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$con) {
+        die('Connection failed: ' . mysqli_connect_error());
+    }
+    
+?>
